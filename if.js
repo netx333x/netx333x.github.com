ad336682b8690e7dd85024848a4fdc55dcd980e0
Nobby <nobby451@gmail.com>
2013-04-01 12:53:29 +0900

    if文による分岐箇所が小さくなるように修正

--- if.js
+++ if.js
@@ -5,9 +5,7 @@
 var now = new Date();
 var hour = now.getHours();
 
-
-if (hour < 12) {
-	window.alert('今は午前' + hour + '時です');
-} else {
-	window.alert('今は午後' + hour + '時です');
-}
+// 分岐する場合は分岐箇所をできるだけ小さくする。
+// 下の記法は三項演算子と言い、簡単な条件で代入するものを変えるときなどに使える。
+var ampm = hour < 12 ? '午前' : '午後';
+window.alert('今は' + ampm + hour + '時です');
